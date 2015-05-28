dd2d99171c648d5eb40d9be99619bb4ea54fde14
Eric Weber <eric@prolificinteractive.com>
2015-05-28 13:42:07 -0400

    Refactor tests and add Parser#method test

--- test/Parser-test.js
+++ test/Parser-test.js
@@ -1,45 +1,67 @@
 var express = require('express');
+var Promise = require('bluebird');
 var should = require('should');
 var htmlToJson = require('../lib/htmlToJson');
 
 describe('Parser', function () {
   describe('provides helper methods', function () {
-    it('runs htmlToJson.parse against the stored filter', function () {
-      var parser = htmlToJson.createParser({
-        'foo': function ($doc) {
-          return $doc.find('#foo').text();
-        }
-      });
+    describe('#parse', function () {
+      it('runs htmlToJson.parse against the stored filter', function () {
+        var parser = htmlToJson.createParser({
+          'foo': function ($doc) {
+            return $doc.find('#foo').text();
+          }
+        });
 
-      return parser.parse('<div id="foo">bar</div>').tap(function (result) {
-        result.foo.should.equal('bar');
+        return parser.parse('<div id="foo">bar</div>').tap(function (result) {
+          result.foo.should.equal('bar');
+        });
       });
     });
 
-    it('runs htmlToJson.request against the stored filter', function (done) {
-      var app = express();
-      var server;
-      var parser = htmlToJson.createParser({
-        'foo': function ($doc) {
-          return $doc.find('#foo').text();
-        }
-      });
+    describe('#method', function () {
+      it('returns a wrapper method for .parse', function () {
+        var parser = htmlToJson.createParser(function ($doc) { return $doc.find('#foo').text(); });
+        var html = '<div id="foo">bar</div>';
+        var method = parser.method();
 
-      function close(err) {
-        server.close(function () {
-          done(err);
+        return Promise
+          .all([
+            parser.parse(html),
+            method(html)
+          ])
+          .tap(function (results) {
+            results[0].should.equal(results[1]);
+          });
+      });
+    });
+
+    describe('#request', function () {
+      it('runs htmlToJson.request against the stored filter', function (done) {
+        var app = express();
+        var server;
+        var parser = htmlToJson.createParser({
+          'foo': function ($doc) {
+            return $doc.find('#foo').text();
+          }
         });
-      }
 
-      app.get('/', function (req, resp) {
-        resp.send('<div id="foo">bar</div>');
-      });
+        function close(err) {
+          server.close(function () {
+            done(err);
+          });
+        }
 
-      server = app.listen(10099, function () {
-        parser.parse('<div id="foo">bar</div>').done(function (result) {
-          result.foo.should.equal('bar');
-          close();
-        }, close);
+        app.get('/', function (req, resp) {
+          resp.send('<div id="foo">bar</div>');
+        });
+
+        server = app.listen(10099, function () {
+          parser.parse('<div id="foo">bar</div>').done(function (result) {
+            result.foo.should.equal('bar');
+            close();
+          }, close);
+        });
       });
     });
   });
